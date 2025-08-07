b0fe212ac4964733a7e3c65d33cfa9339ff67366
Lu Marques <viralata.dev@gmail.com>
2025-08-07 16:07:08 -0300

    Implement ColorSchemeToggle persistence by saving and retrieving the color scheme from local storage. Refactor component to utilize useLayoutEffect for initial setup and useEffect for updates, enhancing user experience with theme consistency.

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,7 +1,28 @@
-import { Button, Group, useMantineColorScheme } from "@mantine/core";
+import {
+	Button,
+	Group,
+	type MantineColorScheme,
+	useMantineColorScheme,
+} from "@mantine/core";
+import { useEffect, useLayoutEffect } from "react";
 
 export function ColorSchemeToggle() {
-	const { setColorScheme } = useMantineColorScheme();
+	const { setColorScheme, colorScheme } = useMantineColorScheme();
+
+	useLayoutEffect(() => {
+		// get from local storage
+		const savedColorScheme = localStorage.getItem("colorScheme");
+		if (savedColorScheme) {
+			setColorScheme(savedColorScheme as MantineColorScheme);
+			return;
+		}
+		setColorScheme("auto");
+	}, [setColorScheme]);
+
+	useEffect(() => {
+		// save to local storage
+		localStorage.setItem("colorScheme", colorScheme);
+	}, [colorScheme]);
 
 	return (
 		<Group justify="end" mt="xl" w="100%" px="xl">
