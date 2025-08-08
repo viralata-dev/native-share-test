f9422b747b22aa2dc5582e75fd6742222851873b
Lu Marques <viralata.dev@gmail.com>
2025-08-08 13:41:14 -0300

    More tweaks

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,9 @@
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+const root = document.getElementById("root");
+if (!root) {
+	throw new Error("Root element not found");
+}
+
+ReactDOM.createRoot(root).render(<App />);
